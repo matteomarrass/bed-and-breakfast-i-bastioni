171c3f1dca4e3d905ea8d2763af13add704c13ae
matteomarrass <matteommarras@gmail.com>
2023-02-14 15:21:19 +0100

    Website completed. Start mobile part

--- src/pages/Rates.jsx
+++ src/pages/Rates.jsx
@@ -1,12 +1,42 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import Video from '../components/Video';
+import Rooms from '../components/Rooms'
 import Footer from "../components/Footer";
 
 function Rates(){
     return(
         <>
         <Navbar />
-            <h2>Rates</h2>
+        <Video />
+        <Rooms />
+            <div className="rates">
+                <p>Camera La Niña (Quadrupla): 100€ per notte</p>
+                <p>Camera La Pinta (Matrimoniale): 70€ per notte</p>
+                <p>Camera La Santa Maria (Matrimoniale): 60€ per notte</p>
+                <p>Camera La Niña (Matrimoniale): 180€ per notte</p>
+            </div>
+            <h2 className="ratesTitle">Per informazioni e prenotazioni compila il seguente form!</h2>
+            <div className="formRates">
+                <form action="">
+                    <label htmlFor="fname">Nome:</label>
+                    <input type="text" id="fname" name="fname" />
+                    <label htmlFor="lname">Cognome:</label>
+                    <input type="text" id="lname" name="lname" />
+                    <label htmlFor='start'>Giorno Arrivo</label>
+                    <input type='date' id="start" placeholder="dd-mm-yyyy"/>
+                    <label htmlFor='end' id="out">Giorno Partenza</label>
+                    <input type='date' id="out" placeholder="dd-mm-yyyy"/>
+                    <label htmlFor="persons">Persone: </label>
+                    <select name="persons" id="persons">
+                       <option>1</option>
+                       <option>2</option>
+                       <option>3</option>
+                       <option>4</option>
+                    </select>
+                    <input type="submit" value="Richiedi ora!" id="submit"/>
+                </form>
+            </div>
         <Footer />
         </>
     )
